c4f3f63c34de2825e783bcbcf0c29b26aa21512a
krecikovsky112 <krecikovsky112@gmail.com>
2021-11-25 13:58:48 +0100

    Implement method to convert all format to JSON format

--- src/main/java/com/example/demo/ConverterString.java
+++ src/main/java/com/example/demo/ConverterString.java
@@ -1,31 +1,60 @@
 package com.example.demo;
 
+import org.json.CDL;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.XML;
 
 public class ConverterString {
     public static String convert(String from, String to, String res) {
-        if(to.equals("txt")){
-            res = convertToFormatTXT(res);
-        }
-        else if(to.equals("csv")){
+        res = convertToJSON(from,res);
 
-        }
-        else if(to.equals("json")){
+        if (to.equals("txt")) {
+            res = convertFromJSONtoTXT(res);
+        } else if (to.equals("csv")) {
+        } else if (to.equals("json")) {
 
-        }
-        else if(to.equals("xml")){
-
+        } else if (to.equals("xml")) {
+            JSONObject json = new JSONObject(res);
+            res = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><stats>";
+            res  += XML.toString(json) + "</stats>";
         }
 
         return res;
     }
 
-    private static String convertToFormatTXT(String res) {
+    private static String convertToJSON(String from, String res) {
+        if(from.equals("txt")){
+            JSONObject jsonObject = new JSONObject();
+            for(int i = 0 ;i<5;i++) {
+                int index = res.indexOf(":");
+                String key = res.substring(0, index);
+                res = res.substring(index + 1);
+                index = res.indexOf("\n");
+                String value = res.substring(0, index);
+                jsonObject.put(key, value);
+                res = res.substring(index + 1);
+            }
+            res = jsonObject.toString();
+        }
+        else if(from.equals("csv")){
+            JSONArray jsonArray = CDL.toJSONArray(res);
+            res = jsonArray.get(0).toString();
+        }
+        else if(from.equals("xml")){
+            JSONObject jsonObject = XML.toJSONObject(res);
+            res = jsonObject.getJSONObject("stats").toString();
+        }
+
+        return res;
+    }
+
+    private static String convertFromJSONtoTXT(String res) {
         JSONObject json = new JSONObject(res);
-        Object lowerCase = json.get("lowerCase");
-        Object upperCase = json.get("upperCase");
+        Object lowerCase = json.get("lowercase");
+        Object upperCase = json.get("uppercase");
         Object numbers = json.get("numbers");
-        Object whiteSpaces = json.get("whiteSpaces");
+        Object whiteSpaces = json.get("whitespaces");
         Object specialCharacters = json.get("specialCharacters");
 
         res = "lowerCase: " + lowerCase + "\n" +
